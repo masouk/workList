960f16986c37f6643d03397921cd5f3a926cc925
sam <anoint216371@hotmail.com>
2014-12-17 18:01:56 +0800

    p

---
+++ css/countdown/countdown.js
@@ -0,0 +1,166 @@
+var countDownHandle;
+		
+	countDownHandle = (function(){
+	// 提示使用者這個模組跟jQuery有相依性
+	if (!window.jQuery) { throw new Error("LikeButtonModule requires jQuery") }
+	
+	// 這行可以增進效能
+	var $ = window.jQuery;
+	
+	var _countDown = function(num){
+		var compose = [];
+		switch(num){
+			case 0:
+				compose = [0,1,2,4,5,6];
+			break;
+			case 1:
+				compose = [2,5];
+			break;
+			case 2:
+				compose = [0,2,3,4,6];
+			break;
+			case 3:
+				compose = [0,2,3,5,6];
+			break;
+			case 4:
+				compose = [1,2,3,5];
+			break;
+			case 5:
+				compose = [0,1,3,5,6];
+			break;
+			case 6:
+				compose = [0,1,3,4,5,6];
+			break;
+			case 7:
+				compose = [0,2,5];
+			break;
+			case 8:
+				compose = [0,1,2,3,4,5,6];
+			break;
+			case 9:
+				compose = [0,1,2,3,5,6];
+			break;
+			default:
+				compose = [0,1,2,4,5,6];
+			break;
+		}
+		return compose;
+	}
+	var _dayDom = function(day){
+		var newDay = "";
+		if(day > 99){
+				newDay = day;
+		}else if(day <= 99 && day > 9){
+				newDay = "0"+day;
+		}else{
+			if(day < 9){
+				newDay = "00"+day;
+			}else{
+				newDay = 999;
+			}
+		}
+		var dayArr = (day.toString(10)).split("");
+		
+							for(i=0;i<3;i++){
+								var numArr = _countDown(parseInt(dayArr[i]));
+								var dom_name = "day0"+(i+1);
+								var dom = $("."+dom_name);
+								_doDom(dom,numArr);
+							}
+							
+	}
+					var _timeDom = function(timeArr){
+						var newTimeArr = [];
+						for(i=0;i<3;i++){
+							if(timeArr[i] <= 9){
+								newTimeArr[i] = "0"+timeArr[i];
+							}else if(timeArr[i] <= 99 && timeArr[i] > 9){
+								newTimeArr[i] = timeArr[i];
+							}else{
+								newTimeArr[i] = "99";
+							}
+						}
+					  
+						for(i=0;i<3;i++){
+							var drawArr = ((newTimeArr[i]).toString(10)).split("");
+							var dom_name;
+							var dom;
+							switch(i){
+								case 0:
+									dom_name = "hours0";
+									break;
+								case 1:
+									dom_name = "minute0";
+									break;
+								case 2:
+									dom_name = "second0";
+									break;
+							}
+							
+							for(j=0;j<2;j++){
+								dom = $("."+dom_name+(j+1));
+								var numArr = _countDown(parseInt(drawArr[j]));
+								_doDom(dom,numArr);
+							}
+						}
+						
+						
+					}
+					var _doDom = function(dom,numArr){
+						
+						dom.find("span[class^=s]").removeClass("active");
+
+						dom.find("span[class^=s]").each(function(key,val){
+							if($.inArray(key,numArr) != -1){
+								$(this).addClass("active");
+							}	
+						})
+						
+					}
+					var _dateDiff = function(date1,date2){
+						var type1 = typeof date1, type2 = typeof date2; 
+						if(type1 == 'string') 
+						date1 = _stringToTime(date1); 
+						else if(date1.getTime) 
+						date1date1 = date1.getTime(); 
+						if(type2 == 'string') 
+						date2 = _stringToTime(date2); 
+						else if(date2.getTime) 
+						date2date2 = date2.getTime(); 
+						return (date1 - date2) / 1000;//结果是秒 
+					}
+					var _stringToTime = function(string){
+						var f = string.split(' ', 2); 
+						var d = (f[0] ? f[0] : '').split('-', 3); 
+						var t = (f[1] ? f[1] : '').split(':', 3); 
+						return (new Date( 
+						parseInt(d[0], 10) || null, 
+						(parseInt(d[1], 10) || 1)-1, 
+						parseInt(d[2], 10) || null, 
+						parseInt(t[0], 10) || null, 
+						parseInt(t[1], 10) || null, 
+						parseInt(t[2], 10) || null 
+						)).getTime(); 
+					}
+					var _refreshTime = function(){
+						var Today=new Date();
+						var nowTime = Today.getFullYear()+"-"+(Today.getMonth()+1)+"-"+Today.getDate()+" "+Today.getHours()+":"+Today.getMinutes()+":"+Today.getSeconds();
+						var dif = _dateDiff("2015-04-01 16:00:00",nowTime) 
+						var day = Math.floor(dif/(24*60*60));
+						var hour = Math.floor((dif%(24*60*60))/(60*60));
+						var min = Math.floor(((dif%(24*60*60))%(60*60))/60);
+						var sec = ((dif%(24*60*60))%(60*60))%60;
+
+						_dayDom(day);
+						var timeArr = [hour,min,sec];
+						_timeDom(timeArr);
+									
+					}
+	return {
+		initialize: function(){
+			// initialization
+									setInterval(_refreshTime, 1000);
+		}
+	};
+	
+}())
--- js/custom_autolayout.js
+++ js/custom_autolayout.js
@@ -6,85 +6,113 @@ readWebData = (function(){
 	
 	// 這行可以增進效能
 	var $ = window.jQuery;
-	var _list = function(){
+	var _list = function(condition){
 		$.ajax({
 			url:"data2.json",
 			dataType:"json",
 			cache:true,
 			timeout:1000*10,
 			success:function(data){
-			var dataCount = data.data.length;
+			var dataCount = 0;
 			var allContent = "";
-			$.map(data.data,function(val,key){
+			var temp = [];
+			var hasAD = [];
+			
+			//過濾有標籤之資料
+			$.each(condition,function(key,val){
+				$.map(data.data,function(val2,key2){
+					if($.inArray(val,val2.type) != -1){
+						hasAD.push(key2);
+					}
+				})
+			})
+			
+			var clearRepeat = $.unique(hasAD);
+			
+			$.map(clearRepeat,function(val,key){
+				temp.push(data.data[val]);
+			})
+			
+			
+			dataCount = temp.length;
+			
+			$.map(temp,function(val,key){
 				var _c = _content;
 				var regex;
 				var outline;
-				
-				if(key%3 == 0){
-					outline = '<div class="row">';
-					_c = outline + _c;
-				}
-				
-				regex=/\.*(\[@NAME@\])+\.*/gi;
-				_c = _c.replace(regex,val.name);
-				console.log(val.name);
-				
-				regex=/\.*(\[@URL@\])+\.*/gi;
-				_c = _c.replace(regex,val.url);
-				console.log(val.url);
-				
-				regex=/\.*(\[@IMAGE@\])+\.*/gi;
-				_c = _c.replace(regex,val.image);
-				console.log(val.image);
-				
-				
-				$.map(val.type,function(val2,key2){
-					regex='[@TYPE@]';
-					var  _t;
-					switch(val2){
-						case "會員":
-							_t = '<span class="label label-primary"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> 會員</span>';
-						break;
-						case "商城":
-							_t = '<span class="label label-success"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> 商城</span>';
-						break;
-						case "金流":
-							_t = '<span class="label label-warning"><span class="glyphicon glyphicon-usd" aria-hidden="true"></span> 金流</span>';
-						break;
-						case "管理後台":
-							_t = '<span class="label label-danger"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> 管理後台</span>';
-						break;
-						case "形象網站":
-							_t = '<span class="label label-default"><span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span> 形象網站</span>';
-						break;
-						case "系統":
-							_t = '<span class="label label-info"><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span> 系統</span>';
-						break;
-						case "活動網站":
-							_t = '<span class="label label-fmactivity"><span class="glyphicon glyphicon-dashboard" aria-hidden="true"></span> 活動網站</span>';
-						break;
-						case "手機":
-							_t = '<span class="label label-fmmobile"><span class="glyphicon glyphicon-phone" aria-hidden="true"></span> 手機</span>';
-						break;
-						case "連結已移除":
-							_t = '<span class="glyphicon glyphicon glyphicon-link" aria-hidden="true" style="color:#f00">連結已移除</span>';
-						break;
-						default:
-							_t = "標籤為定義";
-						break
+	
+					if(key%3 == 0){
+						outline = '<div class="row">';
+						_c = outline + _c;
 					}
-					_c = _c.replace(regex,_t);
-					console.log(val2);
-				})
-				
-				regex=/\.*(\[@TYPE@\])+\.*/gi;
-				_c = _c.replace(regex,"");
+					
+					regex=/\.*(\[@NAME_TW@\])+\.*/gi;
+					_c = _c.replace(regex,val.name_tw);
+					//console.log(val.name_tw);
+					
+					regex=/\.*(\[@NAME_EN@\])+\.*/gi;
+					_c = _c.replace(regex,val.name_en);
+					//console.log(val.name_en);
+					
+					regex=/\.*(\[@URL@\])+\.*/gi;
+					_c = _c.replace(regex,val.url);
+					//console.log(val.url);
+					
+					regex=/\.*(\[@IMAGE@\])+\.*/gi;
+					_c = _c.replace(regex,val.image);
+					//console.log(val.image);
+					
+					
+					$.map(val.type,function(val2,key2){
+						regex='[@TYPE@]';
+						var  _t;
+						switch(val2){
+							case "會員":
+								_t = '<span class="label label-primary"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> 會員</span>';
+							break;
+							case "商城":
+								_t = '<span class="label label-success"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> 商城</span>';
+							break;
+							case "金流":
+								_t = '<span class="label label-warning"><span class="glyphicon glyphicon-usd" aria-hidden="true"></span> 金流</span>';
+							break;
+							case "管理後台":
+								_t = '<span class="label label-danger"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> 管理後台</span>';
+							break;
+							case "形象網站":
+								_t = '<span class="label label-default"><span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span> 形象網站</span>';
+							break;
+							case "系統":
+								_t = '<span class="label label-info"><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span> 系統</span>';
+							break;
+							case "活動網站":
+								_t = '<span class="label label-fmactivity"><span class="glyphicon glyphicon-dashboard" aria-hidden="true"></span> 活動網站</span>';
+							break;
+							case "手機":
+								_t = '<span class="label label-fmmobile"><span class="glyphicon glyphicon-phone" aria-hidden="true"></span> 手機</span>';
+							break;
+							case "連結已移除":
+								_t = '<span class="glyphicon glyphicon glyphicon-link" aria-hidden="true" style="color:#f00">連結已移除</span>';
+							break;
+							default:
+								_t = "標籤為定義";
+							break
+						}
+						_c = _c.replace(regex,_t);
+						//console.log(val2);
+					})
+					
+					regex=/\.*(\[@TYPE@\])+\.*/gi;
+					_c = _c.replace(regex,"");
+					
+			
 				
 				if(key%3 == 2 || dataCount == key+1){
 					outline = '</div>';
 					_c = _c + outline;
 				}
 				allContent += _c;
+
 			})
 				$(".container > .row:last").append(allContent);
 			}
@@ -98,7 +126,8 @@ readWebData = (function(){
 					'</div>'+
 				'</a>'+
 				'<h3>'+
-					'<a href="[@URL@]" target="_blank">[@NAME@]</a>'+
+					'<a href="[@URL@]" target="_blank" class="name_tw">[@NAME_TW@]</a>'+
+					'<a href="[@URL@]" target="_blank" class="name_en">[@NAME_EN@]</a>'+
 				'</h3>'+
 				'<p>'+
 					'[@TYPE@]&nbsp;'+
@@ -115,9 +144,9 @@ readWebData = (function(){
 			'</div>';
 	// the public API interface
 	return {
-		initialize: function(){
+		initialize: function(condition){
 			// initialization
-			_list();
+			_list(condition);
 		}
 	};
 	
